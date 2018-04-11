22f2eaa86b4cca3788e90409724fa12cbd7ba792
dcullender <daniel@blockchain.com>
2018-04-11 10:00:00 +0100

    use supervision strategy for parent actor

--- src/main/kotlin/Part2.kt
+++ src/main/kotlin/Part2.kt
@@ -1,17 +1,47 @@
+
 import akka.actor.AbstractLoggingActor
+import akka.actor.ActorInitializationException
+import akka.actor.ActorKilledException
 import akka.actor.ActorRef
 import akka.actor.ActorSystem
+import akka.actor.AllForOneStrategy
+import akka.actor.DeathPactException
 import akka.actor.Props
+import akka.actor.SupervisorStrategy
 import akka.japi.pf.ReceiveBuilder
+import scala.concurrent.duration.Duration
 
 fun main(args: Array<String>) {
 
     class ChildActor : AbstractLoggingActor() {
 
-        override fun createReceive() = ReceiveBuilder().match(String::class.java) { log().info(it) }.build()
+        override fun preStart() {
+            super.preStart()
+            log().info("Starting Child Actor")
+        }
+
+        override fun createReceive() = ReceiveBuilder().match(String::class.java) {
+            when (it) {
+                "DIE" -> throw Exception("DEAD")
+                else -> log().info(it)
+            }
+        }.build()
     }
 
     class ParentActor : AbstractLoggingActor() {
+
+        /**
+         * restart all child Actors if one throws an exception.
+         */
+        override fun supervisorStrategy() = AllForOneStrategy(-1, Duration.Inf()) {
+            when (it) {
+                is ActorInitializationException -> SupervisorStrategy.stop()
+                is ActorKilledException -> SupervisorStrategy.stop()
+                is DeathPactException -> SupervisorStrategy.stop()
+                else -> SupervisorStrategy.restart()
+            }
+        }
+
         override fun preStart() {
             super.preStart()
             context.actorOf(Props.create(ChildActor::class.java), "child1")
@@ -28,8 +58,7 @@ fun main(args: Array<String>) {
     val actorRef = actorSystem.actorOf(Props.create(ParentActor::class.java), "parent")
     actorSystem.log().info("Sending Hello Kotlin")
     actorRef.tell("Hello Kotlin", ActorRef.noSender())
-
-    // send message by actor selection
-    actorSystem.actorSelection("akka://part2/user/parent/child1").tell("Hello Kotlin", ActorRef.noSender())
-    actorSystem.actorSelection("akka://part2/user/parent/*").tell("Hello Kotlin", ActorRef.noSender())
+    Thread.sleep(1000)
+    actorSystem.log().info("Sending DIE message to child1. We expect all child actors to restart")
+    actorSystem.actorSelection("akka://part2/user/parent/child1").tell("DIE", ActorRef.noSender())
 }
